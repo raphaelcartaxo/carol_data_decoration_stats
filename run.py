2b88e4a003dc7cd9de0cf075403de15ae03f6a9f
raphaelcartaxo <raphael.cartaxo@totvs.com.br>
2021-05-12 11:27:07 -0300

    add the subscription id info

--- run.py
+++ run.py
@@ -30,6 +30,7 @@ settings = app.get_settings(app_name='datadecorationstats')
 os.environ['TECHFINCLIENTID'] = settings['techfinclientid']
 os.environ['TECHFINCLIENTSECRET'] = settings['techfinclientsecret']
 
+
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
@@ -38,73 +39,83 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         exc_type, exc_value, exc_traceback))
     excepthook(exc_type, exc_value, exc_traceback)
 
+
 sys.excepthook = handle_exception
 
 STATS_COLUMNS = [
-  'tenant',
-  'data_model', 
-  'golden_records', 
-  'last_updated_golden_record', 
-  'rejected_records', 
-  'last_updated_rejected_record', 
-  'failed_lookup_records', 
-  'last_updated_failed_lookup_record', 
-  'techfin_records', 
-  'records_diff',
-  'golden_record_stats']
+    'tenant',
+    'data_model',
+    'golden_records',
+    'last_updated_golden_record',
+    'rejected_records',
+    'last_updated_rejected_record',
+    'failed_lookup_records',
+    'last_updated_failed_lookup_record',
+    'techfin_records',
+    'records_diff',
+    'golden_record_stats',
+    'subscription_id']
 
 KEY_MAP = {
-  'apinvoiceaccounting': 'invoiceaccounting_id',
-  'apinvoice': 'invoice_id',
-  'apinvoiceinstallment': 'invoiceinstallment_id',
-  'apinvoicepayments': 'invoicepayments_id',
-  'arinvoiceaccounting': 'invoiceaccounting_id',
-  'arinvoicebra': 'invoicebra_id',
-  'arinvoice': 'invoice_id',
-  'arinvoiceinstallment': 'invoiceinstallment_id',
-  'arinvoiceorigin': '_id',
-  'arinvoicepartner': 'invoicepartner_id',
-  'arinvoicepayments': 'invoicepayments_id',
-  'arpaymentstype': 'transactiontype_id',
-  'cfbankbalance': '_id',
-  'company': 'uuid',
-  'fndbankaccount': 'bankaccount_id',
-  'mdaccount': 'account_id',
-  'mdbankaccount': 'bank_id',
-  'mdbusinesspartnerdocreference': '_id',
-  'mdbusinesspartner': 'businesspartner_id',
-  'mdbusinesspartnergroup': 'businesspartnergroup_id',
-  'mdcostcenter': 'costcenter_id',
-  'mdcurrency': 'currency_id',
-  'mddocreference': 'docreference_id',
-  'mdfinancialcategory': 'financialcategoryid',
-  'organization': 'uuid'}
+    'apinvoiceaccounting': 'invoiceaccounting_id',
+    'apinvoice': 'invoice_id',
+    'apinvoiceinstallment': 'invoiceinstallment_id',
+    'apinvoicepayments': 'invoicepayments_id',
+    'arinvoiceaccounting': 'invoiceaccounting_id',
+    'arinvoicebra': 'invoicebra_id',
+    'arinvoice': 'invoice_id',
+    'arinvoiceinstallment': 'invoiceinstallment_id',
+    'arinvoiceorigin': '_id',
+    'arinvoicepartner': 'invoicepartner_id',
+    'arinvoicepayments': 'invoicepayments_id',
+    'arpaymentstype': 'transactiontype_id',
+    'cfbankbalance': '_id',
+    'company': 'uuid',
+    'fndbankaccount': 'bankaccount_id',
+    'mdaccount': 'account_id',
+    'mdbankaccount': 'bank_id',
+    'mdbusinesspartnerdocreference': '_id',
+    'mdbusinesspartner': 'businesspartner_id',
+    'mdbusinesspartnergroup': 'businesspartnergroup_id',
+    'mdcostcenter': 'costcenter_id',
+    'mdcurrency': 'currency_id',
+    'mddocreference': 'docreference_id',
+    'mdfinancialcategory': 'financialcategoryid',
+    'organization': 'uuid'}
 
 DATA_MODEL_MAP = {
-  "fndHierarchyOrganization" : "company" , 
-  "fndOrganizations" : "organization" , 
-  "mdCurrencies" : "mdcurrency" ,
-  "mdCostCenters" : "mdcostcenter" , 
-  "mdAccounts" : "mdaccount" ,        
-  "mdBankAccounts" : "mdbankaccount" ,  
-  "mdBankAccountsBRA" : "fndbankaccount" ,       
-  "cfBankBalances" : "cfbankbalance" ,
-  "mdPaymentTypes" : "arpaymentstype",   
-  "mdFinancialCategories" : "mdfinancialcategory" ,       
-  "mdBusinessPartnerGroups" : "mdbusinesspartnergroup" , 
-  "mdBusinessPartners" : "mdbusinesspartner" ,     
-  "mdDocReferences" : "mddocreference" , 
-  "mdBusinessPartnerDocReferences" : "mdbusinesspartnerdocreference" , 
-  "arInvoices" : "arinvoice" , 
-  "arInvoiceInstallments" : "arinvoiceinstallment" , 
-  "arInvoicePayments" : "arinvoicepayments" , 
-  "arInvoiceAccountings" : "arinvoiceaccounting" , 
-  "apInvoices" : "apinvoice" , 
-  "apInvoiceInstallments" : "apinvoiceinstallment" ,     
-  "apInvoicePayments" : "apinvoicepayments" , 
-  "apInvoiceAccountings" : "apinvoiceaccounting"}
+    "fndHierarchyOrganization": "company",
+    "fndOrganizations": "organization",
+    "mdCurrencies": "mdcurrency",
+    "mdCostCenters": "mdcostcenter",
+    "mdAccounts": "mdaccount",
+    "mdBankAccounts": "mdbankaccount",
+    "mdBankAccountsBRA": "fndbankaccount",
+    "cfBankBalances": "cfbankbalance",
+    "mdPaymentTypes": "arpaymentstype",
+    "mdFinancialCategories": "mdfinancialcategory",
+    "mdBusinessPartnerGroups": "mdbusinesspartnergroup",
+    "mdBusinessPartners": "mdbusinesspartner",
+    "mdDocReferences": "mddocreference",
+    "mdBusinessPartnerDocReferences": "mdbusinesspartnerdocreference",
+    "arInvoices": "arinvoice",
+    "arInvoiceInstallments": "arinvoiceinstallment",
+    "arInvoicePayments": "arinvoicepayments",
+    "arInvoiceAccountings": "arinvoiceaccounting",
+    "apInvoices": "apinvoice",
+    "apInvoiceInstallments": "apinvoiceinstallment",
+    "apInvoicePayments": "apinvoicepayments",
+    "apInvoiceAccountings": "apinvoiceaccounting"}
 
 DATAMODEL_LIST = list(DATA_MODEL_MAP.values())
+
+
+def get_subscription_id(carol, data_model):
+    dm_id = carol.datamodel.get_by_name(data_model)['mdmId']
+    subscription = carol.call_api(
+        f'v3/subscription/template/{dm_id}', method='GET')
+    return subscription[0]['mdmId']
+
 
 def datetime_logger(text, value=''):
     log = ''
@@ -114,100 +125,117 @@ def datetime_logger(text, value=''):
         log = f'{str(datetime.today())} - {text}'
     logger.info(log)
 
+
 def get_dm(carol, dm_name, max_workers=30, columns=None, max_hits=None, return_metadata=True, merge_records=False, file_pattern=None, callback=None):
 
     df = carol.datamodel.fetch_parquet(
-      dm_name=dm_name, 
-      return_metadata=return_metadata, merge_records=merge_records, 
-      max_workers=max_workers,
-      max_hits=max_hits, columns=columns, 
-      file_pattern=file_pattern 
+        dm_name=dm_name,
+        return_metadata=return_metadata, merge_records=merge_records,
+        max_workers=max_workers,
+        max_hits=max_hits, columns=columns,
+        file_pattern=file_pattern
     )
     return df
+
 
 def get_rejected(carol, dm_name, max_hits=0, max_workers=30, file_pattern=None, remove_duplicates=True, staging_record=True, debug=False):
     df = carol.datamodel.fetch_rejected(
-      dm_name=dm_name, 
-      max_workers=max_workers,
-      max_hits=max_hits, 
-      file_pattern=file_pattern
+        dm_name=dm_name,
+        max_workers=max_workers,
+        max_hits=max_hits,
+        file_pattern=file_pattern
     )
 
     if df.empty:
-      if debug:
-        print(f'no rejected for {dm_name}')
-      return df
+        if debug:
+            print(f'no rejected for {dm_name}')
+        return df
 
     if remove_duplicates:
-      if debug:
-        print(f'{dm_name} before merges {df.shape}')
-      df = drop_duplicated_parquet(df)
+        if debug:
+            print(f'{dm_name} before merges {df.shape}')
+        df = drop_duplicated_parquet(df)
 
     if 'mdmErrors' in df.columns:
-      size = len(df.loc[df.mdmErrors.isna()]['mdmErrors'] )
-      df.loc[df.mdmErrors.isna(),'mdmErrors'] = [{} for _ in range(size)]
+        size = len(df.loc[df.mdmErrors.isna()]['mdmErrors'])
+        df.loc[df.mdmErrors.isna(), 'mdmErrors'] = [{} for _ in range(size)]
     else:
-      if debug: 
-        print('missing mdmErrors column')
+        if debug:
+            print('missing mdmErrors column')
 
     if (staging_record) and ('mdmStagingRecord' in df.columns):
-      size = len(df.loc[df.mdmStagingRecord.isna()]['mdmStagingRecord'] )
-      df.loc[df.mdmStagingRecord.isna(),'mdmStagingRecord'] = [{} for _ in range(size)]
-      aux = pd.DataFrame(df.pop('mdmStagingRecord').tolist())
-      aux.columns = [i.replace('_string','') for i in aux.columns]
-      aux.columns = [i+ '_from_staging' if i.startswith('mdm') else i for i in aux.columns]
-      df = pd.concat([df, aux ,], axis=1)
+        size = len(df.loc[df.mdmStagingRecord.isna()]['mdmStagingRecord'])
+        df.loc[df.mdmStagingRecord.isna(), 'mdmStagingRecord'] = [{}
+                                                                  for _ in range(size)]
+        aux = pd.DataFrame(df.pop('mdmStagingRecord').tolist())
+        aux.columns = [i.replace('_string', '') for i in aux.columns]
+        aux.columns = [
+            i + '_from_staging' if i.startswith('mdm') else i for i in aux.columns]
+        df = pd.concat([df, aux, ], axis=1)
     else:
-      if debug:
-        print('missing mdmStagingRecord column')
-        
+        if debug:
+            print('missing mdmStagingRecord column')
+
     if debug:
-      print(f'{dm_name}  final: {df.shape}')
+        print(f'{dm_name}  final: {df.shape}')
     return df
 
+
 def get_dd_tenants(carol):
-  tenant_list = get_dm(carol, 'caroltenant')
-  tenant_list = tenant_list[(tenant_list.datadecoratio.fillna(False))][['orgname','tenantname']]
-  return tenant_list.drop_duplicates(subset=['orgname','tenantname']).sort_values(['orgname','tenantname'])
+    tenant_list = get_dm(carol, 'caroltenant')
+    tenant_list = tenant_list[(tenant_list.datadecoratio.fillna(False))][[
+        'orgname', 'tenantname']]
+    return tenant_list.drop_duplicates(subset=['orgname', 'tenantname']).sort_values(['orgname', 'tenantname'])
+
 
 def get_filter(setting):
-  filter = None
-  if setting != '':
-    filter = set(setting.split(','))
-  datetime_logger('carol setting',filter)  
-  return filter
+    filter = None
+    if setting != '':
+        filter = set(setting.split(','))
+    datetime_logger('carol setting', filter)
+    return filter
+
 
 def get_techfin_data(org, tenant):
-  techfin_data = list()
-  if (org.lower() == 'totvstechfin'):
-    tf = Techfin()
-    dms_techfin = CarolSyncMonitoring(tf)
-    techfin_data = dms_techfin.get_table_record_count(techfin_app=EnumApps.CASHFLOW.value, carol_tenant=tenant)
-    techfin_data = {DATA_MODEL_MAP[i['tableName']]: i['count'] for i in techfin_data if i['tableName'] in DATA_MODEL_MAP}
-  return techfin_data
+    techfin_data = list()
+    if (org.lower() == 'totvstechfin'):
+        tf = Techfin()
+        dms_techfin = CarolSyncMonitoring(tf)
+        techfin_data = dms_techfin.get_table_record_count(
+            techfin_app=EnumApps.CASHFLOW.value, carol_tenant=tenant)
+        techfin_data = {DATA_MODEL_MAP[i['tableName']]: i['count']
+                        for i in techfin_data if i['tableName'] in DATA_MODEL_MAP}
+    return techfin_data
+
 
 def get_data_model_data(login, tenant, data_model, techfin_data, stats_last_hours):
-    datetime_logger('Data Model', data_model) 
-    data_model_data = []                 
+    datetime_logger('Data Model', data_model)
+    data_model_data = []
     golden_records = 0
     last_updated_golden_record = ''
     rejected_records = 0
     last_updated_rejected_record = ''
-    failed_lookup_records = 0  
+    failed_lookup_records = 0
     last_updated_failed_lookup_record = ''
-    techfin_records = 0  
-    records_diff = 0  
+    techfin_records = 0
+    records_diff = 0
+    subscription_id = ''
 
     golden_data = get_dm(login, data_model)
     golden_records = golden_data.mdmId.nunique()
     last_updated_golden_record = golden_data.mdmLastUpdated.max()
 
-    filtered_data = golden_data[pd.to_datetime(golden_data['mdmStagingCounter']/1000,unit='ms') >= (datetime.utcnow() - timedelta(hours = stats_last_hours))]
-    golden_record_stats = str((pd.to_datetime(filtered_data['mdmCounterForEntity']/1000,unit='ms') - pd.to_datetime(filtered_data['mdmStagingCounter']/1000,unit='ms')).describe().fillna(0)['mean'])
+    subscription_id = get_subscription_id(login, data_model)
 
-    rejected_data = get_rejected(login, data_model, remove_duplicates=True, staging_record=False)
+    filtered_data = golden_data[pd.to_datetime(
+        golden_data['mdmStagingCounter']/1000, unit='ms') >= (datetime.utcnow() - timedelta(hours=stats_last_hours))]
+    golden_record_stats = str((pd.to_datetime(filtered_data['mdmCounterForEntity']/1000, unit='ms') - pd.to_datetime(
+        filtered_data['mdmStagingCounter']/1000, unit='ms')).describe().fillna(0)['mean'])
+
+    rejected_data = get_rejected(
+        login, data_model, remove_duplicates=True, staging_record=False)
     rejected_records = rejected_data.shape[0]
-    if (rejected_records  > 0):
+    if (rejected_records > 0):
         last_updated_rejected_record = rejected_data.mdmLastUpdated.max()
 
     if (rejected_records > 0) and ('mdmSourceType' in rejected_data.columns):
@@ -217,56 +245,62 @@ def get_data_model_data(login, tenant, data_model, techfin_data, stats_last_hour
             last_updated_failed_lookup_record = failed_lookup_data.mdmLastUpdated.max()
 
     if len(techfin_data) > 0:
-      techfin_records = techfin_data.get(data_model,0)
-      records_diff = golden_records - techfin_records
+        techfin_records = techfin_data.get(data_model, 0)
+        records_diff = golden_records - techfin_records
 
-    data_model_data.append([tenant, 
-                            data_model, 
-                            golden_records, 
-                            last_updated_golden_record, 
-                            rejected_records, 
-                            last_updated_rejected_record, 
-                            failed_lookup_records, 
-                            last_updated_failed_lookup_record, 
+    data_model_data.append([tenant,
+                            data_model,
+                            golden_records,
+                            last_updated_golden_record,
+                            rejected_records,
+                            last_updated_rejected_record,
+                            failed_lookup_records,
+                            last_updated_failed_lookup_record,
                             techfin_records,
                             records_diff,
-                            str(golden_record_stats)])
+                            str(golden_record_stats),
+                            subscription_id])
 
     df = pd.DataFrame(data_model_data, columns=STATS_COLUMNS)
     return df
 
+
 def sync_tenant_data(carol, data_model_data):
-  staging = Staging(carol)
-  staging.send_data(staging_name='tenant_stats_data',
-                    data=data_model_data,
-                    force=True,
-                    print_stats=True,
-                    async_send=True,
-                    max_workers=30,
-                    connector_name='datadecoration',
-                    step_size=1000,
-                    auto_create_schema=False,
-                    crosswalk_auto_create=['tenant','data_model'])
+    staging = Staging(carol)
+    staging.send_data(staging_name='tenant_stats_data',
+                      data=data_model_data,
+                      force=True,
+                      print_stats=True,
+                      async_send=True,
+                      max_workers=30,
+                      connector_name='datadecoration',
+                      step_size=1000,
+                      auto_create_schema=False,
+                      crosswalk_auto_create=['tenant', 'data_model'])
+
 
 def data_decoration_stats():
-    datetime_logger('Begin') 
+    datetime_logger('Begin')
     carol = CarolAPI()
     tenant_list = get_dd_tenants(carol)
     carolorgfilter = get_filter(settings['carolorgfilter'])
     caroltenantfilter = get_filter(settings['caroltenantfilter'])
     stats_last_hours = settings['goldenrecordstatslasthours']
 
-
     for tenant in tenant_list.itertuples(index=False):
         if (carolorgfilter is None) or (len(carolorgfilter) > 0) and (tenant.orgname in carolorgfilter):
-          if (caroltenantfilter is None) or (len(caroltenantfilter) > 0) and (tenant.tenantname in caroltenantfilter):
-            datetime_logger('org/tenant', f'{tenant.orgname}/{tenant.tenantname}')
-            with carol.switch_context(env_name=tenant.tenantname, org_name=tenant.orgname, app_name="techfinplatform") as carol_tenant:
-                techfin_data = get_techfin_data(tenant.orgname, tenant.tenantname)
-                for data_model in DATAMODEL_LIST:
-                    data_model_data = get_data_model_data(carol_tenant, tenant.tenantname, data_model, techfin_data, stats_last_hours)
-                    sync_tenant_data(carol, data_model_data)    
-    datetime_logger('End')                         
+            if (caroltenantfilter is None) or (len(caroltenantfilter) > 0) and (tenant.tenantname in caroltenantfilter):
+                datetime_logger(
+                    'org/tenant', f'{tenant.orgname}/{tenant.tenantname}')
+                with carol.switch_context(env_name=tenant.tenantname, org_name=tenant.orgname, app_name="techfinplatform") as carol_tenant:
+                    techfin_data = get_techfin_data(
+                        tenant.orgname, tenant.tenantname)
+                    for data_model in DATAMODEL_LIST:
+                        data_model_data = get_data_model_data(
+                            carol_tenant, tenant.tenantname, data_model, techfin_data, stats_last_hours)
+                        sync_tenant_data(carol, data_model_data)
+    datetime_logger('End')
+
 
 if __name__ == '__main__':
     data_decoration_stats()
